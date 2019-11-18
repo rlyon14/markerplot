612cd3ef09fa176ac1dd09082c9330eddd2b840e
rlyon14 <14910832+rlyon14@users.noreply.github.com>
2019-11-18 06:57:38 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='markerplot',
     description='dev package',
     author='Rick Lyon',
-    author_email='rlyon@firstrf.com',
+    author_email='rlyon14@gmail.com',
     version='0.1.1',
     packages=['markerplot',],
     install_requires=(
